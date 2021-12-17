1a3313f763e066095f7c57bb1c5e3f1576aa062f
wdzajicek <tig.welder4@gmail.com>
2021-12-17 11:00:24 -0600

    Remove gulp and update TargetX form body code.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,2 +1,9 @@
-import './test.js';
-//import './babelTest.js';
+// This SCSS file import allows Webpack to handle SCSS compilation, prefixing, minifying...
+import '../../scss/main.scss';
+
+// window.addEventListener('load', () => {
+//   import('./test').then(({default: test}) => {
+//     test()
+//     return 'another console log'
+//   }).then((string) => console.log(string))
+// });
--- assets/js/src/test.js
+++ assets/js/src/test.js
@@ -1,1 +1,6 @@
 // THIS IS A TEST
+function test() {
+  console.log('This is the test module.')
+}
+
+export default test;
